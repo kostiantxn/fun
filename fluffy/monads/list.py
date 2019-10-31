726d458a723d27172977d90067f7c8e9800d5ca7
Konstantin Ogulchansky <spaceiseternal@gmail.com>
2019-10-31 23:54:48 +0200

    Added support for ranges to `List`.

--- fluffy/monads/list.py
+++ fluffy/monads/list.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 class List:
 
     @classmethod
@@ -26,6 +29,94 @@ class List:
 
         return join(fmap(m, g))
 
+    def __class_getitem__(cls, items: Any):
+        """Creates a `List` instance from a Python list.
+
+        Creates an instance of `List` from the specified value. A value may
+        be either a single value or a tuple. A tuple may have one of the
+        following forms:
+            - [1, 2, 3]: creates a list with values 1, 2 and 3;
+            - [1, ..., 5]: creates a list with values 1, 2, 3, 4 and 5;
+            - [1, 3, ..., 7]: creates a list with values 1, 3, 5 and 7.
+
+        Args:
+            items: A value to create an instance of `List` from.
+
+        Examples:
+            >>> List[1]
+            Node(1, Empty())
+
+            >>> List[1, 2, 3]
+            Node(1, Node(2, Node(3, Empty())))
+
+            >>> List[1, ..., 5]
+            Node(1, Node(2, Node(3, Node(4, Node(5, Empty())))))
+
+            >>> List[1, 3, ..., 9]
+            Node(1, Node(3, Node(5, Node(7, Node(9, Empty())))))
+        """
+
+        if not isinstance(items, tuple):
+            items = (items, )
+
+        def succ(item):
+            if not isinstance(item, int):
+                raise NotImplemented
+
+            return item + 1
+
+        def progression(start, next_, end):
+            if next_ is None:
+                next_ = succ(start)
+
+            item = start
+            step = 0
+
+            while item != next_:
+                item = succ(item)
+                step += 1
+
+            yield start
+            yield item
+
+            while item != end:
+                for _ in range(step - 1):
+                    if (item := succ(item)) == end:
+                        return
+
+                yield (item := succ(item))
+
+        def new(iterable):
+            iterator = iter(iterable)
+            result = None
+            node = None
+
+            while True:
+                try:
+                    item = next(iterator)
+
+                    if result is None:
+                        result = node = Node(item)
+                    else:
+                        node.tail = Node(item)
+                        node = node.tail
+                except StopIteration:
+                    break
+
+            return result or Empty()
+
+        if len(items) == 3:
+            # Sequences like [a, ..., c].
+            if items[1] is ...:
+                return new(progression(items[0], None, items[2]))
+
+        if len(items) == 4:
+            # Sequences like [a, b, ..., c].
+            if items[2] is ...:
+                return new(progression(items[0], items[1], items[3]))
+
+        return new(items)
+
 
 class Node(List):
 
@@ -33,25 +124,11 @@ class Node(List):
         self.value = value
         self.tail = tail or Empty()
 
-    def __str__(self):
-        return f'Node {self.value} ({self.tail})'
+    def __repr__(self):
+        return f'Node({repr(self.value)}, {repr(self.tail)})'
 
 
 class Empty(List):
 
-    def __str__(self):
-        return 'Empty'
-
-
-class L:
-
-    def __class_getitem__(cls, items):
-        """Creates a `List` instance from a Python list."""
-
-        if not isinstance(items, tuple):
-            items = (items, )
-
-        if len(items) == 0:
-            return Empty()
-        else:
-            return Node(items[0], L[items[1:]])
+    def __repr__(self):
+        return 'Empty()'
