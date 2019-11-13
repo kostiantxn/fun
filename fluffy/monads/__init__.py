e0a8c17a97ae6f61049d38989580268f366181ee
Konstantin Ogulchansky <spaceiseternal@gmail.com>
2019-11-13 18:48:22 +0200

    Added `types` to `monads`.

--- fluffy/monads/__init__.py
+++ fluffy/monads/__init__.py
@@ -1,5 +1,2 @@
-from fluffy.monads.io import *
-from fluffy.monads.list import *
-from fluffy.monads.maybe import *
 from fluffy.monads.monad import monad
-from fluffy.monads.result import *
+from fluffy.monads.types import *
